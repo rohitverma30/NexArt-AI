d726cd6f0c50e3cf3d1ee251703bcdd813e45475
rohitverma30 <rv790562@gmail.com>
2024-08-21 18:57:21 +0530

    adding index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
-
+import bodyParser from 'body-parser';
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
+import removeBgRouter from './routes/removeBackground.js';
+import reimagineRouter from './routes/reimagineRouter.js';
+import removeTextRouter from './routes/removeTextRouter.js';
+import upscaleImageRouter from './routes/upscaleImageRouter.js'; 
+import replaceBackgroundRouter from './routes/replaceBackgroundRouter.js';
+import sketchToImageRouter from './routes/sketchToImageRouter.js';
+import authRoutes from './routes/auth.js';
 
 dotenv.config();
 
@@ -14,6 +21,13 @@ app.use(express.json({ limit: '50mb' }));
 
  app.use('/api/v1/post', postRoutes);
  app.use('/api/v1/dalle', dalleRoutes);
+ app.use('/api/v1/remove-background', removeBgRouter); 
+ app.use('/api/v1/reimagine', reimagineRouter);
+ app.use('/api/v1/remove-text', removeTextRouter);
+ app.use('/api/v1/upscale-image', upscaleImageRouter);
+ app.use('/api/v1/replace-background', replaceBackgroundRouter);
+ app.use('/api/v1/sketch-to-image', sketchToImageRouter);
+ app.use('/api/auth', authRoutes);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
